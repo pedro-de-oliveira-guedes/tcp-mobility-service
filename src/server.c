4f8487d912773920cdf68fbd1b73cd9a8a912d7b
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2024-05-04 20:23:07 -0300

    feat: Add server.c file and implement server functionality.

---
+++ src/server.c
@@ -0,0 +1,49 @@
+#include "server.h"
+#include "coordinates.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Server* createServer(char *ipVersion, int port) {
+    Server *server = (Server *) malloc(sizeof(Server));
+
+    server->coordinates.latitude = -19.9227;
+    server->coordinates.longitude = -43.9451;
+    server->ipVersion = ipVersion;
+    server->port = port;
+
+    return server;
+}
+
+void printUsage() {
+    printf("Usage: ./server <ipv4|ipv6> <port>\n");
+    printf("Example: ./server ipv4 50501\n");
+}
+
+Server* parseArguments(int argc, char **argv) {
+    if (argc != 3) {
+        printUsage();
+        exit(1);
+    }
+
+    return createServer(argv[1], atoi(argv[2]));
+}
+
+void printRideOptions(Coordinates *client, Server *server) {
+    double distance = calculateDistance(client, server->coordinates);
+    server->currentDistance = distance;
+
+    printf("========================== CORRIDA DISPONÍVEL ==========================\n");
+    printf("| $ Solicitação a %.2f metros de distância do seu local atual.         |\n", distance);
+    printf("| 0 - Rejeitar corrida                                                 |\n");
+    printf("| 1 - Aceitar corrida                                                  |\n");
+    printf("========================================================================\n");
+}
+
+void handleAcceptRide() {
+    printf("\nCorrida aceita!\n");
+}
+
+void handleRejectRide() {
+    printf("\nCorrida rejeitada!\n");
+}

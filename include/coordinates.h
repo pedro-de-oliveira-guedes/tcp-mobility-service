0bbefe60167df836744f5e54449d6cabe7dc89ab
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2024-05-03 23:30:09 -0300

    Feat: Adding coordinates struct.

---
+++ include/coordinates.h
@@ -0,0 +1,15 @@
+//---------------------------------------------------------------------
+// File	    : coordinates.h
+// Content	: Coordinates TAD definitions
+// Author	: Pedro de Oliveira Guedes
+//---------------------------------------------------------------------
+
+#ifndef COORDINATES
+#define COORDINATES
+
+typedef struct {
+    double latitude;
+    double longitude;
+} Coordinates;
+
+#endif

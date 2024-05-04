748f5e54d9d5ba7903c841628d889b2630c70107
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2024-05-04 17:30:46 -0300

    Feat: Implementing createClient and using it to return a Client when parsing arguments.

--- src/client.c
+++ src/client.c
@@ -3,20 +3,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+Client* createClient(char *ipVersion, char *ipAddress, int port) {
+    Client *client = (Client *) malloc(sizeof(Client));
+
+    client->coordinates.latitude = 0.0;
+    client->coordinates.longitude = 0.0;
+    client->ipVersion = ipVersion;
+    client->ipAddress = ipAddress;
+    client->port = port;
+
+    return client;
+}
+
 void printUsage() {
     printf("Usage: ./client <ipv4|ipv6> <ip_address> <port>\n");
     printf("Example: ./client ipv4 127.0.0.1 50501");
 }
 
-void parseArguments(int argc, char **argv, Client *client) {
+Client* parseArguments(int argc, char **argv) {
     if (argc != 3) {
         printUsage();
         exit(1);
     }
 
-    client->ipVersion = argv[1];
-    client->ipAddress = argv[2];
-    client->port = atoi(argv[3]);
+    return createClient(argv[0], argv[1], atoi(argv[2]));
 }
 
 void printMenu() {

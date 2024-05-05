698f905316e03315e6bc2fa7ec3491121f1df2a6
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2024-05-05 18:13:21 -0300

    Feat: Implementing base server functionality.

--- src/server.c
+++ src/server.c
@@ -1,4 +1,5 @@
 #include "server.h"
+#include "network_config.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +9,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#define BUFF_SIZE 1024
 
 Server* createServer(char *ipVersion, int port) {
     Server *server = (Server *) malloc(sizeof(Server));
@@ -82,4 +85,63 @@ void handleAcceptRide() {
 
 void handleRejectRide() {
     printf("\nCorrida rejeitada!\n");
+}
+
+int main(int argc, char **argv) {
+    Server *server = parseServerArguments(argc, argv);
+
+    // Binds the server socket to the specified address.
+    if (bind(server->socket, (struct sockaddr *)&server->storage, sizeof(server->storage)) < 0) {
+        logError("Erro ao vincular o socket do servidor ao endereço");
+    }
+
+    // Listens for incoming connections on the server socket.
+    if (listen(server->socket, 1) < 0) {
+        logError("Erro ao ouvir por conexões no socket do servidor");
+    }
+
+    // Formats the connection address to string and prints it.
+    char serverAddress[BUFF_SIZE];
+    if (0 != convertAddressToString((struct sockaddr *)&server->storage, serverAddress, BUFF_SIZE)) {
+        logError("Erro ao converter o endereço do servidor para string");
+    }
+    printf("Servidor escutando em %s\n", serverAddress);
+
+    while (1) {
+        // Structures to store the client connection information.
+        struct sockaddr_storage clientStorage;
+        struct sockaddr *clientAddress = (struct sockaddr *)&clientStorage;
+
+        // Accepts the client connection and stores the client address information.
+        socklen_t addrlen = sizeof(clientStorage);
+        int clientSocket = accept(server->socket, clientAddress, &addrlen);
+        if (clientSocket < 0) {
+            logError("Erro ao aceitar a conexão do cliente");
+        }
+
+        // Formats the client address to string and prints it.
+        char clientAddressString[BUFF_SIZE];
+        if (0 != convertAddressToString(clientAddress, clientAddressString, BUFF_SIZE)) {
+            logError("Erro ao converter o endereço do cliente para string");
+        }
+        printf("Conexão aceita de %s\n", clientAddressString);
+
+        // Receives the client coordinates.
+        Coordinates clientCoords;
+        if (recv(clientSocket, &clientCoords, sizeof(Coordinates), 0) == -1) {
+            logError("Erro ao receber as coordenadas do cliente");
+        }
+        printf("Coordenadas do cliente: %.4f, %.4f\n", clientCoords.latitude, clientCoords.longitude);
+
+        // Sends the driver coordinates to the client.
+        if (send(clientSocket, &server->coordinates, sizeof(Coordinates), 0) == -1) {
+            logError("Erro ao enviar as coordenadas do motorista para o cliente");
+        }
+
+        handleDriverOptions(&clientCoords, server);
+    }
+
+    close(server->socket);
+
+    return 0;
 }

76b7017acb1460b00e6c5dbecaf745446b7f5957
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2024-05-05 12:36:21 -0300

    Doc: Adding more comments.

--- src/network_config.c
+++ src/network_config.c
@@ -11,24 +11,26 @@ int parseAddress(char *ipVersion, char *ipAddress, uint16_t port, struct sockadd
         return -1;
     }
 
-    struct in_addr hostAddrIpv4;
+    struct in_addr hostAddrIpv4; // Stores the IPv4 address in network byte order
+    // Checks if the IP version is IPv4 and the IP address is valid, if so, it parses the IP address and port
     if (ipVersion == "ipv4" && inet_pton(AF_INET, ipAddress, &hostAddrIpv4)) {
         struct sockaddr_in *ipv4 = (struct sockaddr_in *) storage;
 
-        ipv4->sin_family = AF_INET;
-        ipv4->sin_port = htons(port);
-        ipv4->sin_addr = hostAddrIpv4;
+        ipv4->sin_family = AF_INET; // Sets the address family to IPv4
+        ipv4->sin_port = htons(port); // Sets the port number
+        ipv4->sin_addr = hostAddrIpv4; // Sets the IP address
 
         return 0;
     }
 
-    struct in6_addr hostAddrIpv6;
+    struct in6_addr hostAddrIpv6; // Stores the IPv6 address in network byte order
+    // Checks if the IP version is IPv6 and the IP address is valid, if so, it parses the IP address and port
     if (ipVersion == "ipv6" && inet_pton(AF_INET6, ipAddress, &hostAddrIpv6)) {
         struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) storage;
 
-        ipv6->sin6_family = AF_INET6;
-        ipv6->sin6_port = htons(port);
-        memcpy(&(ipv6->sin6_addr), &hostAddrIpv6, sizeof(hostAddrIpv6));
+        ipv6->sin6_family = AF_INET6; // Sets the address family to IPv6
+        ipv6->sin6_port = htons(port); // Sets the port number
+        memcpy(&(ipv6->sin6_addr), &hostAddrIpv6, sizeof(hostAddrIpv6)); // Sets the IP address
 
         return 0;
     }

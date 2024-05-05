89de08525d708b255a4ed7c61ad70b8ea9004d2a
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2024-05-05 12:21:31 -0300

    Feat: Implementing network configuration functions.

---
+++ src/network_config.c
@@ -0,0 +1,73 @@
+#include "network_config.h"
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <arpa/inet.h>
+
+int parseAddress(char *ipVersion, char *ipAddress, uint16_t port, struct sockaddr_storage *storage) {
+    if (ipVersion == NULL || ipAddress == NULL) {
+        return -1;
+    }
+
+    struct in_addr hostAddrIpv4;
+    if (ipVersion == "ipv4" && inet_pton(AF_INET, ipAddress, &hostAddrIpv4)) {
+        struct sockaddr_in *ipv4 = (struct sockaddr_in *) storage;
+
+        ipv4->sin_family = AF_INET;
+        ipv4->sin_port = htons(port);
+        ipv4->sin_addr = hostAddrIpv4;
+
+        return 0;
+    }
+
+    struct in6_addr hostAddrIpv6;
+    if (ipVersion == "ipv6" && inet_pton(AF_INET6, ipAddress, &hostAddrIpv6)) {
+        struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) storage;
+
+        ipv6->sin6_family = AF_INET6;
+        ipv6->sin6_port = htons(port);
+        memcpy(&(ipv6->sin6_addr), &hostAddrIpv6, sizeof(hostAddrIpv6));
+
+        return 0;
+    }
+
+    return -1;
+}
+
+int convertAddressToString(struct sockaddr *address, char *str, size_t strSize) {
+    if (address == NULL || str == NULL) {
+        return -1;
+    }
+
+    char addrStr[INET6_ADDRSTRLEN + 1] = "";
+
+    if (address->sa_family == AF_INET) {
+        struct sockaddr_in *ipv4 = (struct sockaddr_in *) address;
+
+        // Convert the IP address to a string
+        if (!inet_ntop(AF_INET, &(ipv4->sin_addr), addrStr, INET6_ADDRSTRLEN + 1))
+            return -1;
+
+        // Formats the IP Address string
+        snprintf(str, strSize, "IPv4 %s %hu", addrStr, ntohs(ipv4->sin_port));
+
+        return 0;
+    }
+
+    if (address->sa_family == AF_INET6) {
+        struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) address;
+
+        // Convert the IP address to a string
+        if (!inet_ntop(AF_INET6, &(ipv6->sin6_addr), addrStr, INET6_ADDRSTRLEN + 1))
+            return -1;
+
+        // Formats the IP Address string
+        snprintf(str, strSize, "IPv6 %s %hu", addrStr, ntohs(ipv6->sin6_port));
+
+        return 0;
+    }
+
+    return -1;
+}
